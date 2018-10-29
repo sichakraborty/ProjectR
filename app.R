0b54be589abff980d855a460f0f0f04e7903805e
sichakraborty <42365198+sichakraborty@users.noreply.github.com>
2018-10-29 12:40:59 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -1,3 +1,6 @@
+rm(list = ls())
+
+library(stringr)
 
 # Read data
 df <- read.csv("gun-violence-data_01-2013_03-2018.csv", stringsAsFactors = FALSE,
@@ -30,7 +33,7 @@ names(df)[names(df) == "n_killed"] <- "Num_Killed"
 names(df)[names(df) == "n_injured"] <- "Num_injured"
 names(df)[names(df) == "gun_stolen"] <- "Gun_Stolen"
 names(df)[names(df) == "location_description"] <- "Location"
-names(df)[names(df) == "n_guns_involved"] <- "Guns_Involved"
+names(df)[names(df) == "n_guns_involved"] <- "Num_Guns_Involved"
 names(df)[names(df) == "participant_age"] <- "Participant_Age"
 names(df)[names(df) == "participant_gender"] <- "Participant_Gender"
 names(df)[names(df) == "participant_relationship"] <- "Participant_Relationship"
@@ -38,5 +41,38 @@ names(df)[names(df) == "participant_status"] <- "Participant_Status"
 names(df)[names(df) == "participant_type"] <- "Participant_Type"
 
 # Change Data Type
+
 df$Date <- as.Date(df$Date)
 df$State <- factor(df$State)
+
+# Search for number of occurrences for the word Stolen to see if there is significant data on whether stolen guns were involved
+
+gunstats <- grep("Stolen", df$Gun_Stolen, fixed = TRUE)
+print(gunstats)
+
+# Rename some data for convenience
+
+df$Gun_Stolen <- gsub("Unknown", "U", df$Gun_Stolen)
+df$Gun_Stolen <- gsub("Stolen", "S", df$Gun_Stolen)
+df$Gun_Stolen <- gsub("Not-stolen", "N", df$Gun_Stolen)
+
+# Add a new column, StolenGunsInvolved, Mark it as Yes if Stolen Guns were involved
+
+df$StolenGunsInvolved <- df$Gun_Stolen
+
+df$StolenGunsInvolved <- ifelse(grepl("S", df$StolenGunsInvolved),"Yes", "No")
+
+# Count the number of stolen guns involved
+
+df$Stolen_Gun_Count <- str_count(df$Gun_Stolen, "S")
+
+#Drop the column
+
+df$Gun_Stolen <- NULL
+
+
+#Participant_Gender
+
+df$Num_Male_Involved <- str_count(df$Participant_Gender, "Male")
+df$Num_Female_Involved <- str_count(df$Participant_Gender, "Female")
+df$Participant_Gender <- NULL 
