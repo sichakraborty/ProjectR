842e15eda9216488a847700e82fb346f30799fb9
sichakraborty <42365198+sichakraborty@users.noreply.github.com>
2018-11-20 21:54:29 -0600

    Add files via upload

--- app-Combined.R
+++ app-Combined.R
@@ -1,8 +1,10 @@
 rm(list = ls())
 
 library(stringr)
-library(qdapRegex)
 library(dplyr)
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(choroplethr))
+library(choroplethrMaps)
 
 # Read data
 df <- read.csv("gun-violence-data_01-2013_03-2018.csv", stringsAsFactors = FALSE,
@@ -25,7 +27,7 @@ df$sources<-NULL
 df$state_house_district<-NULL
 df$state_senate_district<-NULL
 df$participant_name<-NULL
-#df$Location <-NULL
+df$location_description <-NULL
 
 
 # Rename Columns
@@ -36,7 +38,6 @@ names(df)[names(df) == "city_or_county"] <- "City_or_County"
 names(df)[names(df) == "n_killed"] <- "Num_Killed"
 names(df)[names(df) == "n_injured"] <- "Num_injured"
 names(df)[names(df) == "gun_stolen"] <- "Gun_Stolen"
-names(df)[names(df) == "location_description"] <- "Location"
 names(df)[names(df) == "n_guns_involved"] <- "Num_Guns_Involved"
 names(df)[names(df) == "participant_age"] <- "Participant_Age"
 names(df)[names(df) == "participant_gender"] <- "Participant_Gender"
@@ -52,62 +53,52 @@ df$State <- factor(df$State)
 #Remove any row where Num_Gun_Involved is less than 1
 df<- subset(df, Num_Guns_Involved >= 1)  
 
-#Remove any row which does not have participant age
-df<- subset(df, !is.na(Participant_Age))  
+#Remove any row which does not have any participant type
+df<- subset(df, !is.na(Participant_Type))  
 
-# Search for number of occurrences for the word Stolen to see if there is significant data on whether stolen guns were involved
-
-gunstats <- grep("Stolen", df$Gun_Stolen, fixed = TRUE)
-
-
-# Rename some data for convenience
-
-df$Gun_Stolen <- gsub("Unknown", "U", df$Gun_Stolen)
-df$Gun_Stolen <- gsub("Stolen", "S", df$Gun_Stolen)
-df$Gun_Stolen <- gsub("Not-stolen", "N", df$Gun_Stolen)
+#Remove any row which does not have any participant status
+df<- subset(df, !is.na(Participant_Status))  
 
 
 # Count the number of stolen guns involved
 
-df$Stolen_Gun_Count <- str_count(df$Gun_Stolen, "S")
+df$Stolen_Gun_Count <- str_count(df$Gun_Stolen, "Stolen")
 
 #Drop the column
 
 df$Gun_Stolen <- NULL
 
 
-#Participant_Gender
-
-df$Num_Male_Involved <- str_count(df$Participant_Gender, "Male")
-df$Num_Female_Involved <- str_count(df$Participant_Gender, "Female")
-df$Participant_Gender <- NULL 
-
 
 #Participant type
 
-df$Participant_Type <- gsub("Victim", "V", df$Participant_Type)
-df$Participant_Type <- str_count(df$Participant_Type, "V")
+df$Num_Victim <- str_count(df$Participant_Type, "Victim")
+
+df$Num_Suspect <- str_count(df$Participant_Type, "Suspect")
 
 
-## Change name of the column 
-df <- rename(df, Num_Victims = Participant_Type)
+#Add a new columns for participant status "Unharmed" 
+
+df$Num_Unharmed <- (df$Num_Victim+ df$Num_Suspect)-(df$Num_Killed+df$Num_injured)
 
 
+# Total People involved in an incident are those killed, injured and unharmed
+df$People_Involved <- df$Num_Killed + df$Num_injured + df$Num_Unharmed
 
-# Participant Age
+#Drop the status column
+df$Participant_Status <- NULL
 
-df$Participant_Age <- gsub("||", ",", df$Participant_Age, fixed = TRUE)
-df$Participant_Age <- rm_between(df$Participant_Age, "::", ",", extract=TRUE)
-
-
-patternTeen <- "(1[3-9])"
-patternAdults <- "([2-8][0-9]|9[0-9]|100)"
-
-df$Num_Teens_Involved <- str_count(df$Participant_Age,patternTeen )
-df$Num_Adults_Involved <- str_count(df$Participant_Age,patternAdults )
 
 
 #Participant_relationship
+
+# function to match the relationships, will use later
+
+matchRelationship <- function(input = "") {
+  
+  ifelse(grepl(input, tolower(df$Participant_Relationship), fixed = TRUE), "Yes", "No")
+  
+}
 
 df$Acquaintance <- ifelse(
   grepl("family|acquaintance|co-worker|friends|neighbor|significant others", 
@@ -117,52 +108,38 @@ df$Stranger <- ifelse(
   grepl("robbery|random|home invasion", 
         tolower(df$Participant_Relationship)),"Yes", "No")
 
-df$Gang <- ifelse(grepl("gang", tolower(df$Participant_Relationship), fixed = TRUE), "Yes", "No")
+df$Gang <- matchRelationship(input = "gang")
 
-df$Mass_Shooting <-ifelse(grepl("mass shooting", tolower(df$Participant_Relationship), fixed = TRUE), "Yes", "No")
+df$Mass_Shooting <- matchRelationship(input = "mass shooting")
+
+#df$Gang <- ifelse(grepl("gang", tolower(df$Participant_Relationship), fixed = TRUE), "Yes", "No")
+
+#df$Mass_Shooting <-ifelse(grepl("mass shooting", tolower(df$Participant_Relationship), fixed = TRUE), "Yes", "No")
 
 df$Participant_Relationship <- NULL
 
-#People involved
-df$People_Involved <- df$Num_Killed + df$Num_injured
+
+df$Not_Reported <- "No"
+for (i in 1:nrow(df)) {
+  if((df$Acquaintance[i]=="No")&(df$Stranger[i]=="No")&(df$Gang[i]=="No")&(df$Mass_Shooting[i]=="No"))
+    df$Not_Reported[i] <- "Yes"
+  
+  }
 
 
-# trans <- list()
-# childCounter <- 0
-# 
-# for( i in df$Participant_Age)
-# {
-#   childCounter <- 0
-#   
-#   for(j in i)
-#   {
-#     
-#     
-#     ifelse(as.numeric(j)<=12, childCounter <- childCounter+1, 0)
-#     
-#   }
-#   
-#   trans <- append(trans,childCounter)
-#   
-# }
-# 
-# df$Num_Child_Involved <- trans
-# 
-# df$Participant_Age <- NULL
-
-#Add two new columns for participant status "killed" and "injured" and count the number.
-df$Num_participant_killed<-str_count(df$Participant_Status,"Killed")
-#print(identical(df$Num_Killed,df$Num_participant_killed))
-
-df$Num_participant_injured<-str_count(df$Participant_Status,"Injured")
-#print(identical(df$Num_injured,df$Num_participant_injured))
+# Due to inconsistent Data, drop the following column
+df$Participant_Age <- NULL
+df$Participant_Gender <- NULL
 
 
+#Reorder the columns
 
+# Keeping age, gender and status column for ease of comparing. Will drop later
+colnames(df)
+colorder<- c("Incident_Id","Date", "State","City_or_County","Num_Killed","Num_injured","Num_Unharmed","Num_Guns_Involved","Stolen_Gun_Count",
+             "Num_Victim","Num_Suspect","People_Involved","Acquaintance","Stranger",
+             "Gang","Mass_Shooting","Not_Reported")
+df<-df[,colorder]
 
-df$Location <- NULL
-df$Num_Killed <- NULL
-df$Num_injured <- NULL
-df$Participant_Status <- NULL
 
 
